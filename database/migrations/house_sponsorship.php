13410252adb0c2d2463a583d776d67537940f93c
Mandrake091 <wasdgamer91@gmail.com>
2022-07-10 17:41:34 +0200

    create some folders, clean readme.md, create text for migration

---
+++ database/migrations/house_sponsorship.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('house_sponsorship', function (Blueprint $table) {
+            $table->id();
+            $table->date('end_date');
+            $table->foreignId('house_id');
+            $table->foreignId('sponsorship_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
---
+++ database/migrations/table views.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->string('ip_address');
+            $table->foreignId('house_id'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('types');
+    }
+}
