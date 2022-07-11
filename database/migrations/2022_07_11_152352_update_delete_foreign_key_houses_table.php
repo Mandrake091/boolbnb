2a9e33fb3fa4b13e3b95adc5f1bb3ce1ce106fce
NickfuryX <nosinicolo@gmail.com>
2022-07-11 17:58:09 +0200

    fix_register

---
+++ database/migrations/2022_07_11_152352_update_delete_foreign_key_houses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateDeleteForeignKeyHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('houses', function (Blueprint $table) {
+            
+
+            
+            $table->dropForeign('houses_host_id_foreign');
+            $table->dropColumn('host_id');
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('houses', function (Blueprint $table) {
+            
+            $table->foreignId('host_id')->constrained()->onDelete('cascade');
+            // $table->dropForeign('user_id');
+            // $table->dropColumn('user_id');
+            
+    });
+}
+}
