00be90704655aa732e8201edc2b0ad9dc46b7103
NickfuryX <nosinicolo@gmail.com>
2022-07-11 11:19:07 +0200

    Prova 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
                 <div>Prova </div>
+                <div>Prova 2</div>
 
                 <div class="card-body">
                     @if (session('status'))
