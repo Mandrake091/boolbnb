674342716339b814d9a60272a668d583ee4ddb0e
Mandrake091 <wasdgamer91@gmail.com>
2022-07-18 10:31:32 +0200

    start filter for rooms & bed

--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,17 +1,61 @@
 <template>
     <div class="container">
+        <div class="row">
+            <select
+                class="form-select form-select-sm text-center m-auto"
+                @change="changeSelect"
+            >
+                <option value="">Filter by Genre</option>
+                <option
+                    :value="city"
+                    v-for="(city, index) in cities"
+                    :key="index"
+                >
+                    {{ city }}
+                </option>
+            </select>
+        </div>
         <div class="row pt-4">
-            <div class="col-6 col-md-4 col-lg-3 col-xl-2">
-                <div ref="search" id="search">ciao</div>
+            <div
+                class="col-6"
+                v-for="(house, index) in resultsApi"
+                :key="index"
+            >
+                <div class="card" style="width: 24rem">
+                    <img
+                        :src="`/storage/${house.image}`"
+                        class="card-img-top"
+                        alt=""
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ house.title }}</h5>
+                        <p class="card-text">
+                            {{ house.description }}
+                        </p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Città: {{ house.city }}</li>
+                        <li class="list-group-item">
+                            Prezzo a notte {{ house.night_price }}
+                        </li>
+                        <li class="list-group-item">
+                            Tipo: {{ house.type.name }}
+                        </li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
 
-                <input
+                <!-- <input
                     type="text"
                     placeholder="Inserisci Città o Indirizzo"
                     v-model="cityAddress"
                     @change="findGeocoding"
-                />
-                <button @click="print" class="btn btn-primary">Cerca</button>
-                <select
+                /> -->
+                <!-- <button @click="print" class="btn btn-primary">Cerca</button> -->
+                <!-- <select
                     class="form-select form-select-sm text-center m-auto"
                     @change="changeSelect"
                 >
@@ -23,22 +67,7 @@
                     >
                         {{ city }}
                     </option>
-                </select>
-            </div>
-            <div class="col-6 col-md-4 col-lg-3 col-xl-2">
-                <select
-                    class="form-select form-select-sm text-center m-auto"
-                    @change="changeSelect"
-                >
-                    <option value="">Filter by Genre</option>
-                    <option
-                        :value="city"
-                        v-for="(city, index) in cities"
-                        :key="index"
-                    >
-                        {{ city }}
-                    </option>
-                </select>
+                </select> -->
             </div>
         </div>
     </div>
@@ -46,7 +75,7 @@
 
 <script>
 import "../find.js";
-// import { Carousel, Slide } from "vue-carousel";
+
 import { services } from "@tomtom-international/web-sdk-services";
 import SearchBox from "@tomtom-international/web-sdk-plugin-searchbox";
 
@@ -75,17 +104,17 @@ ttSearchBox.on("tomtom.searchbox.resultsfound", function (data) {
 
 export default {
     name: "HomeComponent",
-    components: { 
-    
-    },
+    components: {},
     data() {
         return {
             cities: [],
             cityAddress: "",
             // posts: [],
-            searchApi:
-                "https://api.tomtom.com/search/2/autocomplete/via-cruillas.json?key=HnmOys7lX8qXGsZCcgH6WXEgs8UWaSAh&language=it-IT&limit=10&lat=&lon=&radius=20&countrySet=IT&resultSet=",
+            resultsApi: [],
+
             searchBoxHTML: "",
+            filterGeocode:
+                "https://api.tomtom.com/search/2/geometryFilter.json?key=HnmOys7lX8qXGsZCcgH6WXEgs8UWaSAh&geometryList={geometryList}&poiList={poiList}",
         };
     },
     mounted: function () {},
@@ -128,15 +157,15 @@ export default {
         },
     },
     mounted() {
-        // axios
-        //     .get("/api/houses/")
-        //     .then((res) => {
-        //         this.houses = res.data;
-        //         console.log(this.houses);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
+        axios
+            .get("/api/houses/")
+            .then((res) => {
+                this.resultsApi = res.data;
+                console.log(this.resultsApi);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         // axios.get("/api/posts/").then((res) => {
         //     this.posts = res.data.slice(0, 3);
         //     console.log(this.posts);
