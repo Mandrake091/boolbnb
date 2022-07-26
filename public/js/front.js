41616e7410f9d6b18fc429ce060785be5351e162
Alfo07 <ammiratialfonso07@gmail.com>
2022-07-26 14:52:26 +0200

    modified login and register page

--- public/js/front.js
+++ public/js/front.js
@@ -20475,7 +20475,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/paolo/VsCode/boolbnb/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/progetti-php/boolbnb/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
